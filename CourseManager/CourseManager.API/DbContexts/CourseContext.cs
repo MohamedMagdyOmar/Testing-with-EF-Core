23a9775f894650cf3dc421abd9bdfa102d2e1e63
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-03-31 22:45:57 -0400

    Getting Ready For Unit Test

--- CourseManager/CourseManager.API/DbContexts/CourseContext.cs
+++ CourseManager/CourseManager.API/DbContexts/CourseContext.cs
@@ -9,6 +9,8 @@ namespace CourseManager.API.DbContexts
 {
     public class CourseContext : DbContext
     {
+        // we need to initialize it from unit test with an "In-Memory" Database
+        // so we added below below constructor, to initialize "In-Memory" Database
         public DbSet<Course> Courses { get; set; }
 
         public DbSet<Author> Authors { get; set; }
@@ -17,5 +19,28 @@ namespace CourseManager.API.DbContexts
 
         public CourseContext()
         { }
+
+        // 1- this is prefered options in ASP.Net Core App. it provides these options when registering the context in the "ConfigureServices" Method in the "StartUp" Class
+        // 3- through this constructor, we can setup our "DbContext" from the "Startup" class, when we are actually running the application.
+
+        // 3- also from unit test, you can setup your "DbContext", by initializing it and passing through an options object from unit test directly
+        // 5- there is another way instead of adding below constructor, you can use "OnConfiguring" method that you used in EFCore Course, like below commented method.
+        public CourseContext(DbContextOptions<CourseContext> options): base(options)
+        {
+
+        }
+
+        // 6- this method will be triggered every time we initialize "DbContext", but if we are writing unit test, we use "optionsBuilder.UseInMemoryDatabase", not 
+        // "optionsBuilder.UseSqlServer", so we need to put condition, if it is configured do not configure it again.
+        // in this course we will use "Constructor approach" above, so we are commenting below lines
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server = (localdb)\\mssqllocaldb; Database = CourseManagerDB; Trusted_Connection = True;");
+            }
+        }
+        */
     }
 }
--- CourseManager/CourseManager.API/Startup.cs
+++ CourseManager/CourseManager.API/Startup.cs
@@ -1,7 +1,9 @@
-﻿using CourseManager.API.Services;
+﻿using CourseManager.API.DbContexts;
+using CourseManager.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,13 +23,24 @@ namespace CourseManager.API
         {
             //services.AddMvc(option => option.EnableEndpointRouting = false).AddNewtonsoftJson();
             services.AddMvc(option => option.EnableEndpointRouting = false);
-            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddScoped<AuthorRepository>();
+
+            services.AddDbContext<CourseContext>(options =>
+            {
+                // 2- behind the scenes, when the database is first accessed, these options will passed to the constructor you created in "CourseContext".
+                // if you set breakpoint here and in the constructor in "CourseContext", you will fins that below line will call the constructor in "CourseContext"
+                // and will pass to it the below "ConnectionString".
+                // then this constructor will call "Main" function in "Program.cs"
+                //options.UseSqlServer(@"Server = (localdb)\\mssqllocaldb; Database = CourseManagerDB; Trusted_Connection = True;");
+
+                //4- we call "UseInMemoryDatabase" instead of above "UseSqlServer" for unit testing,
+                options.UseInMemoryDatabase("CourseManagerInMemoryDB");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) 
         {
             if (env.IsDevelopment())
             {
